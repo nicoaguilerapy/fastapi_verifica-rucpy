2ea04afc61d337c3adfa97704922808eed46dadb
Nicolás Aguilera <60054614+nicoaguilerapy@users.noreply.github.com>
2024-10-03 13:44:02 -0400

    New Calcula DV

--- main.py
+++ main.py
@@ -14,6 +14,35 @@ P12_FOLDER = "./p12/"
 
 set_client = SifenApi()
 
+def calcular_dv_11(p_numero: str) -> int:
+    # Calcular el dígito verificador - RUC (módulo 11)
+    p_basemax = 9
+    v_numero_al = ""
+    
+    # Extraer solo los números, y convertir caracteres a números si no son dígitos
+    for v_caracter in p_numero:
+        if '0' <= v_caracter <= '9':
+            v_numero_al += v_caracter
+        else:
+            v_numero_al += str(ord(v_caracter))  # Convertir no dígitos a su valor numérico
+
+    k = 2
+    v_total = 0
+    i = len(v_numero_al) - 1
+
+    # Recorrer el string de derecha a izquierda
+    while i >= 0:
+        k = 2 if k > p_basemax else k
+        v_numero_aux = int(v_numero_al[i])  # Convertir el carácter a entero
+        v_total += v_numero_aux * k
+        k += 1
+        i -= 1
+
+    # Cálculo del módulo 11
+    v_resto = v_total % 11
+    v_digit = 11 - v_resto if v_resto > 1 else 0
+
+    return v_digit
 
 @app.get("/ruc/{documento}")
 def verificar_documento(documento: str):
@@ -52,6 +81,8 @@ def verificar_documento(documento: str):
             passphase=cert_pass,
             config=None
         )
+        
+        print(resultado)
 
         if resultado != None and resultado['success']:
             respuesta['result']['respuesta_codigo'] = resultado['data']['ns2:rResEnviConsRUC']['ns2:dCodRes']
@@ -59,6 +90,7 @@ def verificar_documento(documento: str):
             try:
                 respuesta['result']['ruc_razonsocial'] = resultado['data']['ns2:rResEnviConsRUC']['ns2:xContRUC']['ns2:dRazCons']
                 respuesta['result']['ruc_estado'] = resultado['data']['ns2:rResEnviConsRUC']['ns2:xContRUC']['ns2:dDesEstCons']
+                respuesta['result']['ruc_ruc'] = "{}-{}".format(documento, calcular_dv_11(documento))
                 respuesta['success'] = True
             except:
                 pass
